8e30f415afd7b6b4441e704d750c337cb4be0d58
Marc Hanheide <marc@hanheide.net>
2024-01-31 21:01:00 +0800

    more tests with ddsrouter

--- .devcontainer/setup-router.sh
+++ .devcontainer/setup-router.sh
@@ -1,4 +1,9 @@
+source /opt/ros/lcas/install/setup.bash
+source /workspaces/limo_ros2/install/setup.bash
+
 unset RMW_IMPLEMENTATION
 unset ROS_LOCALHOST_ONLY
 export FASTRTPS_DEFAULT_PROFILES_FILE=/workspaces/limo_ros2/.devcontainer/super_client_configuration_file.xml
 export ROS_DISCOVERY_SERVER=localhost:11888
+unset ROS_DISCOVERY_SERVER
+unset FASTRTPS_DEFAULT_PROFILES_FILE
