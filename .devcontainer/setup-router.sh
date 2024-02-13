0a7a30f4ec59b6f2bb8b5a638231bc1dbe487d9e
Marc Hanheide <marc@hanheide.net>
2024-02-13 07:15:31 +0000

    Separate dev (#17)
    * work towards cyclone
    
    * ignore stuff in .local
    
    * install all
    
    * disable PR build for arm
    
    * set up ros better

--- .devcontainer/setup-router.sh
+++ .devcontainer/setup-router.sh
@@ -1,9 +1,43 @@
-source /opt/ros/lcas/install/setup.bash
-source /workspaces/limo_ros2/install/setup.bash
+#source /opt/ros/lcas/install/setup.bash
+#source /workspaces/limo_ros2/install/setup.bash
 
 unset RMW_IMPLEMENTATION
 unset ROS_LOCALHOST_ONLY
-export FASTRTPS_DEFAULT_PROFILES_FILE=/workspaces/limo_ros2/.devcontainer/super_client_configuration_file.xml
-export ROS_DISCOVERY_SERVER=localhost:11888
-unset ROS_DISCOVERY_SERVER
-unset FASTRTPS_DEFAULT_PROFILES_FILE
+RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+
+#export FASTRTPS_DEFAULT_PROFILES_FILE=/workspaces/limo_ros2/.devcontainer/super_client_configuration_file.xml
+#export ROS_DISCOVERY_SERVER=localhost:11888
+
+
+read -r -d '' CYCLONEDDS_URI << EOM
+<?xml version="1.0" encoding="UTF-8" ?>
+<CycloneDDS xmlns="https://cdds.io/config" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="https://cdds.io/config https://raw.githubusercontent.com/eclipse-cyclonedds/cyclonedds/master/etc/cyclonedds.xsd">
+    <Domain id="0">
+        <General>
+            <AllowMulticast>false</AllowMulticast>
+            <MaxMessageSize>65500B</MaxMessageSize>
+        <FragmentSize>4000B</FragmentSize>
+        <Transport>udp</Transport>
+        </General>
+    <Discovery>
+        <Peers>
+            <Peer address="localhost"/>
+            <Peer address="`hostname`"/>
+            <!--<Peer address="[IPV6-address]"/>-->
+        </Peers>
+        <ParticipantIndex>auto</ParticipantIndex>
+    </Discovery>
+        <Internal>
+            <Watermarks>
+                <WhcHigh>500kB</WhcHigh>
+            </Watermarks>
+        </Internal>
+        <Tracing>
+            <Verbosity>info</Verbosity>
+            <OutputFile>stdout</OutputFile>
+        </Tracing>
+    </Domain>
+</CycloneDDS>
+EOM
+
+export CYCLONEDDS_URI
